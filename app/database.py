4ac1db17c7be45a2220f9eaf227ac2911b68d213
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-04-21 21:48:10 +0530

    Error

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/fastapi"
